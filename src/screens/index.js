b4b0995788215fe7c7e5e0103288e0268b8a791c
rxprasetya <rafidarthurprasetya@gmail.com>
2023-11-13 15:13:41 +0700

    BAB 6

---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import Discover from "./Discover";
+import Bookmark from "./Bookmark";
+import Profile from "./Profile";
+import BlogDetail from "./BlogDetail";
+export {Home, Discover, Bookmark, Profile, BlogDetail}
